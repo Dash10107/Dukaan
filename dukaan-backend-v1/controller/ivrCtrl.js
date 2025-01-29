19f0f30581a00cc7e98e10f0f842437e92f4b967
Vinay Vora <voravinay82@gmail.com>
2025-01-30 04:25:10 +0530

    better deploy

--- dukaan-backend-v1/controller/ivrCtrl.js
+++ dukaan-backend-v1/controller/ivrCtrl.js
@@ -25,10 +25,10 @@ const handleIncomingMessage = async (req, res) => {
     const userPhoneNumber = From.replace('whatsapp:+91', '');
     
     let user = await User.findOne({ mobile: userPhoneNumber.toString() });
-    if (!user) {
+    if (!user) {``
       console.log(`New user detected: ${userPhoneNumber}`);
       
-      await sendWhatsAppMessage(From, `Please create user from your phone no!  ${userPhoneNumber} ${typeof userPhoneNumber}`);
+      await sendWhatsAppMessage(From, `Please create user from your phone no!  ${userPhoneNumber} ${typeof userPhoneNumber}, ${user}`);
       return res.status(200).send('OK');
     }
 
